3adf47384b5360ae37b9a0d51f2b652bf7389724
kchen07 <32498223+kchen07@users.noreply.github.com>
2017-11-02 17:04:15 -0700

    done

--- ikid/ikid/DadJokeViewController.swift
+++ ikid/ikid/DadJokeViewController.swift
@@ -9,19 +9,42 @@
 import UIKit
 
 class DadJokeViewController: UIViewController {
-
+    var answer = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var nextBack: UIButton!
+    @IBOutlet weak var questionBox: UILabel!
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var answerImg: UIImageView!
+    
+    @IBAction func ShowAnswer(_ sender: Any) {
+        UIView.beginAnimations("Flip", context: nil)
+        UIView.setAnimationDuration(0.5)
+        UIView.setAnimationCurve(.easeInOut)
+        if (!answer) {
+            questionBox.text = nil
+            answerImg.image = UIImage(named: "chicken")
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache:true)
+            nextBack.setTitle("back", for: .normal)
+            answer = true
+        } else {
+            answer = false
+            questionBox.text = "Which size does the chicken have the most feathers?"
+            answerImg.image = nil
+            nextBack.setTitle("back", for: .normal)
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache:true)
 
+            
+        }
+    }
     /*
     // MARK: - Navigation
 
--- ikid/ikid/FirstViewController.swift
+++ ikid/ikid/FirstViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-    
+    var answer = false
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +22,24 @@ class FirstViewController: UIViewController {
     }
     
     @IBAction func showAnswer(_ sender: Any) {
+        UIView.beginAnimations("flip", context: nil)
+        UIView.setAnimationDuration(0.5)
+        UIView.setAnimationCurve(.easeInOut)
+        if (!answer) {
+            questionBox.text = nil
+            answerImg.image = UIImage(named: "Image")
+            UIView.setAnimationTransition(.flipFromLeft, for: view, cache: true)
+            nextBack.setTitle(("back"), for: .normal)
+            answer = true
+        } else {
+            answerImg.image = nil
+            questionBox.text = "What do you call a fish without an eye?"
+            answer = false
+            nextBack.setTitle("next", for: .normal)
+            UIView.setAnimationTransition(.flipFromRight, for: view, cache: true)
+
+        }
+        UIView.commitAnimations()
         
     }
     override func didReceiveMemoryWarning() {
